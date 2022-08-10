4516b32a9dc7d589f5fb5134dc9aa48a1e11a91c
Tiberius Lockett <tlockett680@gmail.com>
2022-08-10 10:45:21 -0700

    Add basic html, label and id, js input

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+window.onload = function() {
+
+  let form = document.querySelectory('form');
+  form.onsubmit = fucntion(event) {
+
+  event.preventDefault();
+  const personInput = document.getElementById("personInput").value;
+  
+  }
+}
